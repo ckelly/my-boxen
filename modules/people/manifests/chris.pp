d1028ddc54fbc5a0132111185e59c3cc31f903d6
Chris Kelly <chris@ckelly.net>
2015-01-26 01:57:53 -0500

    more customizations

--- modules/people/manifests/chris.pp
+++ modules/people/manifests/chris.pp
@@ -1,48 +1,8 @@
 class people::chris {
-
-  include dropbox
-  include chrome
-  include java
-  include firefox
-  include vlc
-  include spotify
-  include sublime_text
-  include wget
-  include zsh
-
-  #  class { 'intellij':
-  #    edition => 'ultimate',
-  #     version => '13.1.1'
-  #  }
-
-  #include virtualbox
-  #include vagrant
-
-  #
-  # Remove services we don't want
-  #
-  #service {"dev.nginx":
-  #    ensure => "stopped",
-  #}
-
-  $my_home  = "/Users/${::luser}"
-  $projects = "${my_home}/dev/src"
-
-  file { $projects:
-    ensure => directory,
-  }
-
-  $dotfiles = "${projects}/dotfiles"
-
-  repository { $dotfiles:
-    source  => 'ckelly/dotfiles',
-    require => File[$projects],
-    notify  => Exec['ckelly-make-dotfiles'],
-  }
-
-  exec { 'ckelly-make-dotfiles':
-    command     => "cd ${dotfiles} && make",
-    refreshonly => true,
-  }
+  include people::chris::params
+  include people::chris::applications
+  include people::chris::dotfiles
+  include people::chris::osx_defaults
+  include people::chris::python_dev
 }
 
